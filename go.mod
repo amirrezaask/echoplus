6c93e279fcf366edc3844d352ead9e96228ee4fa
amirrezaask <raskarpour@gmail.com>
2021-07-10 22:30:33 +0430

    initial commit

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/amirrezaask/echoplus
+
+go 1.16
+
+require (
+	github.com/labstack/echo v3.3.10+incompatible // indirect
+	github.com/labstack/echo/v4 v4.3.0 // indirect
+	github.com/labstack/gommon v0.3.0 // indirect
+	golang.org/x/crypto v0.0.0-20210616213533-5ff15b29337e // indirect
+)
---
+++ model_binder.go
@@ -0,0 +1,35 @@
+package echoplus
+
+import "github.com/labstack/echo/v4"
+
+// func main() {
+// 	e := echo.New()
+// 	type RootModel struct {
+// 		ID string `json:"id"`
+// 	}
+
+// 	e.Use(MakeMiddlewareFrom(func(ctx echo.Context) (interface{}, error) {
+// 		id := ctx.Param("id")
+// 		return &RootModel{ID: id}, nil
+// 	}))
+// 	e.GET("/:id", func(ctx echo.Context) error {
+// 		req := ctx.Get("Model").(*RootModel)
+// 		return ctx.JSON(200, map[string]interface{}{"id": req.ID})
+// 	})
+// 	if err := e.Start(":8080"); err != nil {
+// 		panic(err)
+// 	}
+// }
+
+func MakeMiddlewareFrom(binder func(ctx echo.Context) (interface{}, error)) echo.MiddlewareFunc {
+	return func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(ctx echo.Context) error {
+			model, err := binder(ctx)
+			if err != nil {
+				return err
+			}
+			ctx.Set("Model", model)
+			return next(ctx)
+		}
+	}
+}
